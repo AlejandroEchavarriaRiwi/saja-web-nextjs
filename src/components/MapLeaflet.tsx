00f4b7ba4e6ee27c84b5b184cc5aaf84458e21be
AlejandroEchavarriaRiwi <aechavarriaj@gmail.com>
2025-04-15 15:38:18 -0500

    fix map

--- src/components/MapLeaflet.tsx
+++ src/components/MapLeaflet.tsx
@@ -5,9 +5,9 @@ import L from 'leaflet';
 // Configurar iconos de marcador
 delete (L.Icon.Default.prototype as any)._getIconUrl;
 L.Icon.Default.mergeOptions({
-  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
-  iconUrl: require('leaflet/dist/images/marker-icon.png'),
-  shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
+    iconRetinaUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png',
+    iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png',
+    shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png',
 });
 
 interface MapLeafletProps {
@@ -25,7 +25,7 @@ const MapLeaflet: React.FC<MapLeafletProps> = ({ lat, lng }) => {
     <MapContainer center={[lat, lng]} zoom={13} style={mapContainerStyle}>
       <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
       <Marker position={[lat, lng]}>
-        <Popup>Ubicación: [{lat}, {lng}]</Popup>
+        <Popup>Aquí estamos!</Popup>
       </Marker>
     </MapContainer>
   );
