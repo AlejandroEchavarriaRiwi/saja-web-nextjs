f853779b32fa071e2d077f47624b01fc746463ab
AlejandroEchavarriaRiwi <aechavarriaj@gmail.com>
2025-04-15 15:55:54 -0500

    fix location

--- src/components/ui/cards/location.tsx
+++ src/components/ui/cards/location.tsx
@@ -14,7 +14,6 @@ const MapLeaflet = dynamic(() => import('../../MapLeaflet'), {
 const LocationCard: React.FC<LocationCardProps> = ({ title, lat, lng }) => {
   return (
     <div className="flex flex-col items-center justify-center p-4 bg-white rounded-lg shadow-md dark:bg-gray-800 w-full mb-8">
-      <h2 className="text-xl font-bold text-gray-800 dark:text-white mb-2">{title}</h2>
       <MapLeaflet lat={lat} lng={lng} />
     </div>
   );
